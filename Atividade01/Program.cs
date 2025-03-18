9d25b551ab1980ff517d062578e477896bf0e44c
Agatha Sates <satesagatha@gmail.com>
2025-03-18 15:29:34 -0300

    zerado para começar a refazer com a academia

--- Atividade01/Program.cs
+++ Atividade01/Program.cs
@@ -6,18 +6,8 @@ namespace Atividade01
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Write.MainTitle();
-            while (true)
-            {
-                Write.MainMenu();
-                int menuOption = Validators.IsInt(1, 2);
-                switch (menuOption) 
-                {
-                    case 1: Triangle.NewTriangle(); break;
-                    case 2: Write.Goodbye(); return;
-                }                              
-            }
+        {                             
+           
         }
     }
 }
